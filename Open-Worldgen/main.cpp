4efec7bbe3d184bba312a052a91bee047badf1cd
Joseph Bourque <indegon1@gmail.com>
2018-01-18 18:03:23 -0600

    Created basic GUI window for project and added camera controls

--- Open-Worldgen/main.cpp
+++ Open-Worldgen/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include "GlWindow.h"
 using std::cout;
-int main() {
-	cout << "Hello World!\n";
-	system("Pause");
-	return 0;
+using std::endl;
+int main(int argc, char **argv) {
+	// GUI variables (mostly for position and size)
+	int winWidth = 800;
+	int winHeight = 800;
+	int winXPos = winWidth / 2;
+	int winYPos = winHeight / 2;
+
+	// Set up our graphics window
+	GlWindow *mainWindow = new GlWindow(0, 0, winWidth, winHeight, "");
+	mainWindow->redraw();
+	mainWindow->label("Open Worldgen v1.0");
+	mainWindow->end();
+
+	// Make the window visible
+	mainWindow->show(argc, argv);
+	return Fl::run();
 }
